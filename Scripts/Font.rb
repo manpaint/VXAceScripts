a632e609808a156ba10557135ea99aa64c59b4f0
manpaint <45153555+manpaint@users.noreply.github.com>
2022-02-13 17:43:50 -0500

    Create Font.rb

---
+++ Scripts/Font.rb
@@ -0,0 +1,45 @@
+#=========================================
+#Script police d'Ecriture
+#=========================================
+#
+#  Auteur : Brandobscure.
+#
+#=======================================
+#
+# Script à placer au dessus du script Main.
+# Vous pouvez modifier le script a la ligne : 
+# 32 pour le Nom de l'écriture
+# 43 pour la taille de l'écriture
+# 49 si vous voulez mettre l'écriture en gras
+# 53 si vous voulez le mettre en italique                                      
+#    
+#===============================================================
+#
+#
+# Choix de la police d'ecriture pour le jeu.
+# Font.default_name = ["papyrus"]
+#
+# Vous pouvez placer plusieurs police ecriture en cas ou si le pc d'un joueur
+# na pas la police demandé. Voici donc le morceau de code à changer.
+#-------------------------------------------------
+# Font.default_name = ["Rod","nom de la 2em police"]
+#---------------------------------------------------
+
+Font.default_name = ["alttp"]
+
+#Taille de l'ecriture de la police.
+Font.default_size= 20
+
+#Option pour les polices d'ecriture.
+#false=desactivé; true=activé
+
+#Ecriture en gras.
+Font.default_bold = true
+
+
+#Ecriture en italique
+Font.default_italic = false
+
+#Ombre: Activé par défaut.
+
+Font.default_shadow = false
